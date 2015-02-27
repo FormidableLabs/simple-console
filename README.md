4aae322e8aee4e78348dd30537b13388e37d5204
Ryan Roemer <ryan@loose-bits.com>
2015-02-27 13:54:31 -0800

    Refactor to working.

--- README.md
+++ README.md
@@ -10,6 +10,40 @@ Provides a simple, but useful cross-browser-compatible `console` logger.
 * Buildable from straight CommonJS source.
 * Available as bundled distributions as well.
 
+### Installation
+
+#### AMD
+
+```js
+define(["simple-console"], function (SimpleConsole) {
+  var con = new SimpleConsole();
+  con.log("Hello world!");
+});
+```
+
+#### CommonJS
+
+```js
+var SimpleConsole = require("simple-console");
+var con = new SimpleConsole();
+con.log("Hello world!");
+```
+
+#### VanillaJS
+
+In your HTML:
+
+```html
+<script src="PATH/TO/simple-console/dist/simple-console.umd.js"></script>
+```
+
+In your JS:
+
+```js
+var con = new window.SimpleConsole();
+con.log("Hello world!");
+```
+
 ### Also See
 
 Similar projects that can help with `console`:
@@ -20,3 +54,10 @@ Similar projects that can help with `console`:
 ### License
 Copyright 2015 Formidable Labs, Inc.
 Released under the [MIT](./LICENSE.txt) License,
+
+[trav]: https://travis-ci.org/
+[trav_img]: https://api.travis-ci.org/FormidableLabs/simple-console.svg
+[trav_site]: https://travis-ci.org/FormidableLabs/simple-console
+[cov]: https://coveralls.io
+[cov_img]: https://img.shields.io/coveralls/FormidableLabs/simple-console.svg
+[cov_site]: https://coveralls.io/r/FormidableLabs/simple-console
--- simple-console.js
+++ simple-console.js
@@ -3,64 +3,88 @@
  * --------------
  * A small, cross-browser-friendly `console` wrapper.
  */
-// Patches
-var EMPTY_OBJ = {};
-var NOOP = function () {};
+(function (root) {
+  // Memoizations.
+  var _console;
 
-// Console attributes
-var props = ["memory"];
-var meths = (
-  "assert,clear,count,debug,dir,dirxml,error,exception,group," +
-  "groupCollapsed,groupEnd,info,log,markTimeline,profile,profiles,profileEnd," +
-  "show,table,time,timeEnd,timeline,timelineEnd,timeStamp,trace,warn"
-).split(",");
+  // Patches
+  var EMPTY_OBJ = {};
+  var NOOP = function () {};
 
-// Memoizations.
-var _console;
+  // Console attributes
+  var props = ["memory"];
+  var meths = (
+    "assert,clear,count,debug,dir,dirxml,error,exception,group," +
+    "groupCollapsed,groupEnd,info,log,markTimeline,profile,profiles," +
+    "profileEnd,show,table,time,timeEnd,timeline,timelineEnd,timeStamp,trace" +
+    ",warn"
+  ).split(",");
 
-/**
- * Console abstraction.
- */
-var Console = module.exports = function () {
-  var con = this._getConsole();
-  var noConsole = !!con;
-  con = con || {};
-  var i;
+  /**
+   * Console abstraction.
+   */
+  var SimpleConsole = function () {
+    var self = this;
+    var con = this._getConsole();
+    var noConsole = !con;
+    con = con || {};
+    var i;
 
-  // Patch properties, methods.
-  for (i = 0; i < props.length; i++) {
-    this[props[i]] = con[props[i]] || EMPTY_OBJ;
-  }
-  for (i = 0; i < meths.length; i++) {
-    // First level patch.
-    this[meths[i]] = con[meths[i]] || NOOP;
+    // Patch properties, methods.
+    for (i = 0; i < props.length; i++) {
+      this[props[i]] = con[props[i]] || EMPTY_OBJ;
+    }
 
     // Enable "apply" and "bind" on methods by converting to real function.
     // See: http://patik.com/blog/complete-cross-browser-console-log/
-    if (typeof this[meths[i]] !== "function") {
-      (function (self, meth) {
-        if (!!Function.prototype.bind) {
-          // IE9: Bind to our create real function.
-          self[meth] = Function.prototype.bind.call(self[meth], self);
+    for (i = 0; i < meths.length; i++) {
+      (function (meth) {
+        if (noConsole || !con[meth]) {
+          // No console or method: Noop it.
+          self[meth] = NOOP;
+        } else if (Function.prototype.bind) {
+          // IE9 and most others: Bind to our create real function.
+          // Should work if `console.FOO` is `function` or `object`.
+          self[meth] = Function.prototype.bind.call(con[meth], con);
         } else {
           // IE8: No bind, so even more tortured.
           self[meth] = function () {
-            Function.prototype.call.call(self[meth], self,
+            Function.prototype.call.call(con[meth], con,
               Array.prototype.slice.call(arguments));
           };
         }
-      })(this, meths[i]);
+      })(meths[i]);
+    }
+  };
+
+  /**
+   * Accessor to console object. (Cached).
+   *
+   * @returns {Object} the console object or `null` if unavailable.
+   */
+  SimpleConsole.prototype._getConsole = function () {
+    if (typeof _console !== "undefined") { return _console; }
+    _console = window.console || null;
+    return _console;
+  };
+
+  // UMD wrapper: Borrowed from webpack version.
+  function umd() {
+    if (typeof exports === "object" && typeof module === "object") {
+      // CommonJS
+      module.exports = SimpleConsole;
+    } else if (typeof define === "function" && define.amd) {
+      // AMD
+      define(function () {
+        return SimpleConsole;
+      });
+    } else {
+      // VanillaJS / Old exports
+      var mod = typeof exports === "object" ? exports : root;
+      mod.SimpleConsole = SimpleConsole;
     }
   }
-};
 
-/**
- * Accessor to console object. (Cached).
- *
- * @returns {Object} the console object or `null` if unavailable.
- */
-Console.prototype._getConsole = function () {
-  if (typeof _console !== "undefined") { return _console; }
-  _console = window.console || null;
-  return _console;
-};
+  // Wrap.
+  umd(SimpleConsole);
+})(this);
