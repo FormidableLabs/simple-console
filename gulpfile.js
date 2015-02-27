83ea951061ed4f8c72f488abba66f48872e64fec
Ryan Roemer <ryan@loose-bits.com>
2015-02-27 14:45:14 -0800

    Start adding the karma stuff.

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,11 @@
 /**
  * Gulp file.
  */
+var _ = require("lodash");
 var gulp = require("gulp");
 var eslint = require("gulp-eslint");
 var jscs = require("gulp-jscs");
+var karma = require("gulp-karma");
 var connect = require("gulp-connect");
 
 // ----------------------------------------------------------------------------
@@ -67,6 +69,136 @@ gulp.task("chk", function () {
 });
 
 // ----------------------------------------------------------------------------
+// Tests
+// ----------------------------------------------------------------------------
+// Use `node_modules` Phantom
+process.env.PHANTOMJS_BIN = "./node_modules/.bin/phantomjs";
+
+// Sauce labs environments.
+var SAUCE_ENVS = {
+  /*eslint-disable camelcase*/
+  // Already tested in Travis.
+  // sl_firefox: {
+  //   base: "SauceLabs",
+  //   browserName: "firefox"
+  // },
+  sl_chrome: {
+    base: "SauceLabs",
+    browserName: "chrome"
+  }
+  // sl_safari: {
+  //   base: "SauceLabs",
+  //   browserName: "safari",
+  //   platform: "OS X 10.9"
+  // },
+  // sl_ie_9: {
+  //   base: "SauceLabs",
+  //   browserName: "internet explorer",
+  //   platform: "Windows 7",
+  //   version: "9"
+  // },
+  // sl_ie_10: {
+  //   base: "SauceLabs",
+  //   browserName: "internet explorer",
+  //   platform: "Windows 7",
+  //   version: "10"
+  // },
+  // sl_ie_11: {
+  //   base: "SauceLabs",
+  //   browserName: "internet explorer",
+  //   platform: "Windows 7",
+  //   version: "11"
+  // }
+  /*eslint-enable camelcase*/
+};
+
+// SauceLabs tag.
+var SAUCE_BRANCH = process.env.TRAVIS_BRANCH || "local";
+var SAUCE_TAG = process.env.SAUCE_USERNAME + "@" + SAUCE_BRANCH;
+
+// Karma coverage.
+var KARMA_COV = {
+  reporters: ["spec", "coverage"],
+  preprocessors: {
+    "simple-console.js": ["coverage"]
+  },
+  coverageReporter: {
+    reporters: [
+      { type: "json", file: "coverage.json" },
+      { type: "lcov" }
+    ],
+    dir: "coverage/"
+  }
+};
+
+// Test wrapper.
+var testFrontend = function () {
+  var files = [
+    // Libraries
+    "node_modules/expect.js/index.js",
+    "node_modules/sinon/pkg/sinon.js",
+
+    // App files
+    "simple-console.js",
+
+    // Tests
+    "test/spec/simple-console.spec.js"
+  ];
+
+  var opts = _.extend.apply(_, [{
+    frameworks: ["mocha"],
+    port: 9999,
+    reporters: ["spec"],
+    client: {
+      mocha: {
+        ui: "bdd"
+      }
+    }
+  }].concat(_.toArray(arguments)));
+
+  return function () {
+    return gulp
+      .src(files)
+      .pipe(karma(opts))
+      .on("error", function (err) {
+        throw err;
+      });
+  };
+};
+
+gulp.task("test:frontend:dev", testFrontend({
+  singleRun: true,
+  browsers: ["PhantomJS"]
+}, KARMA_COV));
+
+gulp.task("test:frontend:ci", testFrontend({
+  singleRun: true,
+  browsers: ["PhantomJS", "Firefox"]
+}, KARMA_COV, {
+  reporters: ["spec", "coverage"]
+}));
+
+gulp.task("test:frontend:sauce", testFrontend({
+  singleRun: true,
+  reporters: ["spec", "saucelabs"],
+  sauceLabs: {
+    testName: "simple-console - Frontend Unit Tests",
+    tags: [SAUCE_TAG],
+    "public": "public"
+  },
+  // Timeouts: Allow "n" minutes before saying "good enough". See also:
+  // https://github.com/angular/angular.js/blob/master/karma-shared.conf.js
+  captureTimeout: 0, // Pass through to SL.
+  customLaunchers: SAUCE_ENVS,
+  browsers: Object.keys(SAUCE_ENVS)
+}));
+
+gulp.task("test:frontend:all", testFrontend({
+  port: 9998,
+  browsers: ["PhantomJS", "Firefox", "Chrome", "Safari"]
+}));
+
+// ----------------------------------------------------------------------------
 // Servers
 // ----------------------------------------------------------------------------
 // Static server.
@@ -81,7 +213,7 @@ gulp.task("server", function () {
 // ----------------------------------------------------------------------------
 // Aggregations
 // ----------------------------------------------------------------------------
-gulp.task("check", ["chk", "eslint", "jscs"]);
-gulp.task("check:ci", ["chk", "eslint", "jscs"]);
+gulp.task("check", ["chk", "eslint", "jscs", "test:frontend:dev"]);
+gulp.task("check:ci", ["chk", "eslint", "jscs", "test:frontend:ci"]);
 gulp.task("dev", ["server"]);
 gulp.task("default", ["check", "dev"]);
